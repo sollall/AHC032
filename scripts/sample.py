0d93edd1304aa8af4a50b51add0634b9554482bd
sollall <komugikoakasya@gmail.com>
2025-03-25 23:21:27 +0900

    テストした　変数のrenameもした

--- scripts/sample.py
+++ scripts/sample.py
@@ -6,7 +6,7 @@ MOD=998244353
 def score(A):
     return np.sum(A%MOD)
 
-def push_stamp(A, stamp, top_left=(0, 0)):
+def push_stamp(board, stamp, top_left=(0, 0)):
     """
     A の指定した位置に stamp を重ねて加算する関数（インプレース処理で高速）
     
@@ -21,13 +21,14 @@ def push_stamp(A, stamp, top_left=(0, 0)):
     # B を配置する開始位置
     r, c = top_left
     # A の該当部分に B を加算（インプレースで実行）
-    A[r:r+stamp.shape[0], c:c+stamp.shape[1]] += stamp
-    return A  # 変更後の A を返す（A 自体も更新される）
+    board[r:r+stamp.shape[0], c:c+stamp.shape[1]] += stamp
+    return board  # 変更後の A を返す（A 自体も更新される）
 
 def solve(epsilon,cooling_rate,epoch):
     N,M,K=map(int,input().split())
 
     A=[list(map(int,input().split())) for _ in range(N)]
+    A=np.array(A,dtype=np.int64)
 
     stamps=[]
     for _ in range(M):
@@ -35,8 +36,10 @@ def solve(epsilon,cooling_rate,epoch):
         for _ in range(3):
             stamp.append(list(map(int,input().split())))
         stamps.append(stamp)
-    
-    print(stamps)
+    stamps=np.array(stamps,dtype=np.int64)
+
+    alterA=push_stamp(A.copy(),stamps[0],(1,1))
+
 
     return epoch
 
